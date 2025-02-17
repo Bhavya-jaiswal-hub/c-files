367cf76dd6c7394a6c1d2a4e3a045eb5a38adbf8
bhavya <bhavyajaiswal71@gmail.com>
2025-02-17 23:15:34 +0530

    modification in the problem

--- strivers problem/longestsubarraywith k sum/brute.cpp
+++ strivers problem/longestsubarraywith k sum/brute.cpp
@@ -1,35 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
- int main () {
-         int n;
-         int k;
-         int num; 
-         int len = 0;
-       vector<int> nums;
-        cin >> n ;
-         cout << endl;
-          cin>> k ;
-           cout << endl;
-           for(int i= 0 ; i < n; i++) {
-               cin >> num;
-               nums.push_back(num);
-             
-           }
-       for(int i=0; i<n; i++) {
-         for(int j =i; j< n; j++){
-             int sum = 0;
-              for (k = i; k< j ; k++) {
-                    sum = sum + nums[k];
-                    if(sum == k) {
-                         len = max(len,j - i +1);
-                    }
-              }
-         }
-       }
-    cout << "The maximum subarrray is " ;
-    cout << endl ;
-     cout << len ;
 
+int main() {
+    int n, k, num, len = 0;
+    vector<int> nums;
+    
+    cin >> n;
+    cin >> k;
+    
+    for (int i = 0; i < n; i++) {
+        cin >> num;
+        nums.push_back(num);
+    }
 
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            int sum = 0;
+            for (int m = i; m <= j; m++) {  
+                sum += nums[m];
+            }
+            if (sum == k) {
+                len = max(len, j - i + 1);
+            }
+        }
+    }
+
+    cout << "The maximum subarray length is: " << len << endl;
     return 0;
- }
+}
