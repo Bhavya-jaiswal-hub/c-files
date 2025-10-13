077ff1802909b4f2e8d17eebe05f3508d479155d
bhavya <bhavya2313139@akgec.ac.in>
2025-10-13 18:35:44 +0530

    practise the delete the node in the linked list

--- Linked list/basics/basic.cpp
+++ Linked list/basics/basic.cpp
@@ -1,53 +1,74 @@
-#include<bits/stdc++.h>
+#include<bits/stdc++.h> 
 using namespace std;
 
-class  Node {
-     public:
-     int data;
-     Node* next;
+struct Node {
+      public: 
+      int data;
+      Node* next;
 
-     public: 
-     Node( int data1, Node* next1){
-         data = data1;
-         next = next1;
-     }
+      public: 
+      Node(int data1, Node* next1){
+           data = data1;
+           next = next1;
 
-     public: 
-     Node(int data1){
-         data = data1;
-         next = nullptr;    
-     }
-    
+      }
+        public: 
+      Node(int data1){
+           data = data1;
+           next = nullptr;
+
+      }
 };
-
- Node* convertArrtoLL(vector<int> &arr){
-        Node* head  = new Node(arr[0]);
-        Node* mover =  head;
-        for(int i = 1; i< arr.size(); i++){
-             Node* temp = new Node(arr[i]);
-             mover-> next = temp;
-             mover = temp;
-
-        }
-        return head;
+Node* convertArr2LL(vector<int> &arr){
+     Node* head = new Node(arr[0], nullptr);
+     Node* mover = head;
+     for(int i = 1; i<arr.size(); i++) { 
+         Node* temp = new Node(arr[i]);
+         mover->next = temp;
+         mover = temp;
      }
-// note we can use the class word in place of  the struct 
+     return head;
+}
 
-int main() {
-     vector<int> arr  = {5,2,3,4};
-    //  Node* y = new Node(arr[2], nullptr);
-    // cout <<  y ;
-    // cout << endl;
-    // cout << y->data; 
-    Node* head = convertArrtoLL(arr);
-    cout << head->data;
-    cout << endl;
-    Node* temp = head;
-    while(temp) {
-         cout << temp->data << " ";
-         temp = temp->next;
-         
-    }
+Node* removesHead(Node* head) {
+     Node* temp;
+     temp = head;
+     head = head->next;
+     delete temp;
+     return head;
+}
 
+Node* removeTail(Node* head){
+     Node* temp = head;
+     if(head->next == NULL || head == NULL) {
+         return NULL;
+     }
+     while(temp->next->next != NULL) {
+         temp  = temp->next;
+     }
+     delete temp->next;
+     temp->next = nullptr;
 
+     return head;
+
+}
+
+int main(){
+    vector<int> arr  = {2,3,4,5};
+     Node* head = convertArr2LL(arr);
+    // Node* y = new Node(arr[0],nullptr);
+    // cout  << y->data; 
+
+    // delete the head of the linked list 
+     
+    //   head  = removesHead(head);
+    //   cout << head->data;
+
+      // delete the tail of the linked list;
+
+    // head = removeTail(head);
+    // cout << head->next->next->data;
+
+    // delete the kth element of the linked list 
+    
 }
