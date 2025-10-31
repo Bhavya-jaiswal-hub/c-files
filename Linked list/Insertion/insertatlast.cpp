4c5f01ca0a1599689a933af5b2a4e5a8ff692ce2
bhavya <bhavya2313139@akgec.ac.in>
2025-10-31 15:58:45 +0530

    added the new code

---
+++ Linked list/Insertion/insertatlast.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node {
+    public:
+    int data;
+    Node* next;
+    public:
+   Node(int data1, Node* next1){
+     int data = data1;
+     Node* next = next1;
+   }
+    Node(int data1) {
+         data = data1;
+     }
+    
+};
+
+Node* ArrtoLL(vector<int> & arr){
+     Node* head = new Node(arr[0]);
+     Node* mover = head;
+     for(int i  = 0; i< arr.size(); i++){
+         Node* temp = new Node(arr[i]);
+         mover->next = temp;
+         mover = temp;
+
+
+     }
+
+     return head;
+}
+
+Node* insertLast(Node* head , int val){
+     if(head == NULL) {
+         return new Node(val);
+
+     }
+    Node* temp = head;
+    while(temp->next != NULL){
+         temp = temp->next;
+    }
+
+    Node* newNode = new Node(val);
+    temp->next = newNode;
+    return head;
+}
+
+void printList(Node* head) {
+    while (head != NULL) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+
+int main() {
+     vector<int> arr = {2,3,4,5};
+     Node* head = ArrtoLL(arr);
+     head = insertLast(head, 100);
+     printList(head);
+     
+}   
+
+
